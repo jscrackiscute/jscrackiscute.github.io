630c9875be96b56347dd9657c2f084729ff9cd00
jscrackiscute <jscrackiscute@gmail.com>
2026-01-08 05:11:08 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    // Change this password to something secure before going online!
+    // ADMIN_PASSWORD removed. Using server-side validation.
+
+    // Site Name Settings
+    SITE_NAME: "Donut SMP Cheats",
+
+    // Discord Link
+    DISCORD_URL: "https://discord.gg/7gJsapQUsV"
+};
